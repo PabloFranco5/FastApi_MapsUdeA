28e01c829408f7b5c7dbf607fbe00860d6593702
Natalia Mosquera <natmosqu@bancolombia.com.co>
2022-11-29 21:21:50 -0500

    codificación de contraseña db

--- funciones.py
+++ funciones.py
@@ -1,14 +1,21 @@
 import psycopg2
 import requests 
 import urllib
-import datetime 
-from datetime import date, datetime
+from app.v1.utils.settings import Settings
 
-conexion1 = psycopg2.connect(database="BaseDatos_MapsUdeA", user="postgres", password="1000895134j")
-cur = conexion1.cursor()
+settings = Settings()
+
+DB_NAME = settings.db_name
+DB_USER = settings.db_user
+DB_PASS = settings.db_pass
+DB_HOST = settings.db_host
+DB_PORT = settings.db_port
+
+conexion = psycopg2.connect(database="BaseDatos_MapsUdeA", user="postgres", password="1000895134j")
+cur = conexion.cursor()
 cur.execute( "SELECT punto_salida,punto_llegada FROM usuario" )
 query2= cur.fetchall()
-conexion1.close()
+conexion.close()
 print(query2[-1]) #ultimo dato de lainterfaz
 origen = query2[-1][0]   #dato de salida de la interfaz
 destino = query2[-1][1]  #dato de llegada de la interfaz
@@ -54,13 +61,13 @@ def clima(destino):
     clima_o = responseclima_o.json()
     #clima_d = responseclima_d.json()
 
-    temperatura_origen = clima_o["current_condition"][0]["temp_C"]
+    temperatura_destino = clima_o["current_condition"][0]["temp_C"]
     descripcion = clima_o["current_condition"][0]["lang_es"][0]["value"]
-    return temperatura_origen, descripcion
+    return temperatura_destino, descripcion
 
 def main(): 
     temperatura_destino, descripcion = clima(destino)
-    print(f"La temperatura actual de {destino} es de {temperatura_destino} °C. {descripcion}")
+    #print(f"La temperatura actual de {destino} es de {temperatura_destino} °C. {descripcion}")
     return f"La temperatura actual de {destino} es de {temperatura_destino} °C. {descripcion}"
 
 
@@ -68,16 +75,13 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-
-conexion1 = psycopg2.connect(database="BaseDatos_MapsUdeA", user="postgres", password="1000895134j")
+conexion1 = psycopg2.connect(database=DB_NAME, user=DB_USER, password= DB_PASS)
 cursor1=conexion1.cursor()
-#sql="insert into resumen_viaje(lugar_salida,lugar_destino,tiempo_trayecto,distancia_recorrida,clima) values (%s,%s,%s,%s,%s)"
+
 p= main()
 datos=( origen, destino, a["Duración del viaje"], a["Distacia a Recorrer"], p )
-print(datos)
-sql= f"insert into resumen_viaje({datos[0]},{datos[1]},{str(datos[2])},{str(datos[3])},{datos[4]})"
+sql=f"""insert into resumen_viaje(lugar_salida,lugar_destino,tiempo_trayecto,distancia_recorrida,clima) 
+values ({datos[0]},{datos[1]},{datos[2]},{datos[3]},{datos[4]})"""
 cursor1.execute(sql, datos)
 conexion1.commit()
 conexion1.close()
